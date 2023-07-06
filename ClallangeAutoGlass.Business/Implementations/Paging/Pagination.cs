8e03562df8cd187207671232c2a5c7f3151af249
João Nascimento <joaonascimento@MacBook-Air-de-Joao.local>
2023-07-06 20:51:33 -0300

    Pagination class

---
+++ ClallangeAutoGlass.Business/Implementations/Paging/Pagination.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ClallangeAutoGlass.Business.Implementations.Paging
+{
+    public class Pagination
+    {
+
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+
+            set
+            {
+                _pageSize = (value <= 0) ? _pageSize : value;
+            }
+        }
+
+    }
+}
+
--- ClallangeAutoGlass.Business/Interfaces/Repositories/IRepository.cs
+++ ClallangeAutoGlass.Business/Interfaces/Repositories/IRepository.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Linq.Expressions;
-using ClallangeAutoGlass.Business.Entities;
+﻿using ClallangeAutoGlass.Business.Entities;
+using ClallangeAutoGlass.Business.Implementations.Paging;
 
 namespace ClallangeAutoGlass.Business.Interfaces.Repositories
 {
     public interface IRepository<TEntity> : IDisposable where TEntity : BaseEntity
     {
         Task Add(TEntity entity);
-        Task<List<TEntity>> ObterTodos();
+        Task<List<TEntity>> GetAll(Pagination? pagination = null);
         Task Update(TEntity entity);
         Task Remove(Guid id);
         Task<int> SaveChanges();
